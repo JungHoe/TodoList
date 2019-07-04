9e97d4e9beb4a3d87314b3c66894592acf068478
hyune7142 <hyune7142@daum.net>
2019-07-04 12:19:46 +0900

    opengraph update

---
+++ another-react-todo-master/src/components/OpengraphReactComponent.js
@@ -0,0 +1,298 @@
+import React, { Component } from 'react';
+import styles from './OpengraphReactComponent.css';
+import currencyMappings from './currencyMappings';
+
+export default class OpengraphReactComponent extends Component {
+  state = {
+    result: null,
+    error: null,
+  };
+
+  componentDidMount() {
+    if (this.props.dontMakeCall) {
+      this.setState({ result: this.props.result });
+    } else {
+      const { useProxy, fullRender, forceCacheUpdate } = this.props;
+      const acceptLang = 'auto';
+      const appId = this.props.appId;
+      const site = encodeURIComponent(this.props.site);
+      let url = `https://opengraph.io/api/1.1/site/http%3A%2F%2F${site}?accept_lang=${acceptLang}&app_id=${appId}`;
+
+      if (useProxy) {
+        url = url + '&use_proxy=true';
+      }
+      if (forceCacheUpdate) {
+        url = url + '&cache_ok=false';
+      }
+      if (fullRender) {
+        url = url + '&full_render=true';
+      }
+
+      fetch(url)
+        .then(res => res.json())
+        .then(result => {
+          console.log(result);
+          if (!result.error) {
+            this.setState({ result });
+          } else {
+            console.error(result);
+          }
+        });
+    }
+  }
+
+  getResultsToUse = () => {
+    let { openGraph, htmlInferred, hybridGraph } = this.state.result;
+    // protect against null values
+    openGraph = openGraph || {};
+    htmlInferred = htmlInferred || {};
+    hybridGraph = hybridGraph || {};
+
+    const site_name = openGraph.site_name || htmlInferred.site_name || hybridGraph.site_name;
+    const title = openGraph.title || htmlInferred.title || hybridGraph.title;
+    const url = openGraph.url || htmlInferred.url || hybridGraph.url;
+    const favicon = htmlInferred.favicon || hybridGraph.favicon;
+    const description = openGraph.description || htmlInferred.description || hybridGraph.description;
+    const products = hybridGraph.products || htmlInferred.products;
+
+    let image;
+    if (openGraph.image) {
+      image = openGraph.image.url;
+    } else {
+      image = htmlInferred.image || hybridGraph.image;
+    }
+
+    let video;
+    if (openGraph.video) {
+      video = openGraph.video.secure_url;
+    } else {
+      video = hybridGraph.video || htmlInferred.video;
+    }
+
+    return {
+      site_name,
+      title,
+      url,
+      favicon,
+      image,
+      description,
+      video,
+      products,
+    };
+  };
+
+  renderStarsForRating = rating => {
+    let bestRating = parseInt(rating.bestRating) || 0;
+    let ratingValue = parseFloat(rating.ratingValue) || 0;
+
+    let starArr = [];
+    for (let i = 0; i < bestRating; i++) {
+      if (i < ratingValue) {
+        starArr.push(
+          <i key={`star-${i}`} className={'coloredStar'}>
+            ★
+          </i>,
+        );
+      } else {
+        starArr.push(<i key={`star-${i}`}>★</i>);
+      }
+    }
+
+    return (
+      <p>
+        {starArr}
+        <span className={'greyText'}>{` ${rating.reviewCount} reviews`}</span>
+      </p>
+    );
+  };
+
+  truncateDescription = description => {
+    description = description || '';
+    if (description.length < 125) {
+      return description;
+    } else {
+      description = description.slice(0, 123);
+      description += '...';
+      return description;
+    }
+  };
+
+  renderPrice = (offer, resultsToUse) => {
+    let price = offer.price || offer.highPrice || offer.lowPrice;
+    if (!offer) {
+      return false;
+    } else {
+      let currencyMatch = currencyMappings[offer.currency];
+      return (
+        <p>
+          {!this.props.hidePrice && <span className={'priceText'}>{`${!!currencyMatch ? currencyMatch.symbol : ''}${price}`}</span>}
+          <span className={'greyText'}>{` from ${resultsToUse.site_name}`}</span>
+        </p>
+      );
+    }
+  };
+
+  renderSmallProduct = resultsToUse => {
+    let goodProduct = resultsToUse.products.find(p => !!p.name);
+    let goodOffer = goodProduct.offers[0];
+    let imageSrc;
+    if (goodProduct.images && goodProduct.images.length > 1) {
+      imageSrc = goodProduct.images[0];
+    } else {
+      imageSrc = resultsToUse.image;
+    }
+
+    return (
+      <div className="outerWrapperSmall">
+        <div>
+          <div className={'imgWrapperSmall'}>
+            <img className={'responsiveImage'} src={imageSrc} alt={'alt'} />
+          </div>
+        </div>
+        <div className={'textWrapperSmall'}>
+          <div className={'siteNameLinkWrapper'}>
+            <a href={resultsToUse.url}>{goodProduct.name}</a>
+          </div>
+          <div className={'titleWrapper'}>{this.renderPrice(goodOffer, resultsToUse)}</div>
+          {!!goodProduct.totalRating && this.renderStarsForRating(goodProduct.totalRating)}
+          <p>{this.truncateDescription(goodProduct.description)}</p>
+        </div>
+      </div>
+    );
+  };
+
+  renderLargeProduct = (resultsToUse, imageClassName) => {
+    let goodProduct = resultsToUse.products.find(p => !!p.name);
+    let goodOffer = goodProduct.offers[0];
+    let imageSrc;
+    if (goodProduct.images && goodProduct.images.length > 1) {
+      imageSrc = goodProduct.images[0];
+    } else {
+      imageSrc = resultsToUse.image;
+    }
+
+    return (
+      <div className="wrapperLarge">
+        <div className={'imgWrapperLarge'}>
+          <img className={imageClassName} src={imageSrc} alt={'alt'} />
+        </div>
+        <div className={'textWrapperLarge'}>
+          <div className={'siteNameLinkWrapper'}>
+            <a href={resultsToUse.url}>{goodProduct.name}</a>
+          </div>
+          <div className={'titleWrapper'}>{this.renderPrice(goodOffer, resultsToUse)}</div>
+          {!!goodProduct.totalRating && this.renderStarsForRating(goodProduct.totalRating)}
+          <p>{this.truncateDescription(goodProduct.description)}</p>
+        </div>
+      </div>
+    );
+  };
+
+  renderLarge = resultsToUse => {
+    let imageClassName = this.props.spin ? 'responsiveImage App-logo' : 'responsiveImage';
+
+    let feature = null;
+
+    if (resultsToUse.products && !this.props.dontUseProduct) {
+      return this.renderLargeProduct(resultsToUse, imageClassName);
+    } else if (resultsToUse.video && !this.props.dontUseVideo) {
+      feature = (
+        <div className={'imgWrapperLarge'}>
+          <iframe
+            className={'responsiveVideo'}
+            src={resultsToUse.video}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      );
+    } else {
+      feature = (
+        <div className={'imgWrapperLarge'}>
+          <img className={imageClassName} src={resultsToUse.image} alt={'alt'} />
+        </div>
+      );
+    }
+
+    return (
+      <div className="wrapperLarge">
+        {feature}
+        <div className={'textWrapperLarge'}>
+          <div className={'siteNameLinkWrapper'}>
+            <a href={resultsToUse.url}>{resultsToUse.site_name}</a>
+          </div>
+          <div className={'titleWrapper'}>
+            <p>{resultsToUse.title}</p>
+          </div>
+          <p>{resultsToUse.description}</p>
+        </div>
+      </div>
+    );
+  };
+
+  renderSmall = resultsToUse => {
+    let string = '';
+    if (resultsToUse.products && !this.props.dontUseProduct) {
+      return this.renderSmallProduct(resultsToUse);
+    } else {
+      return (
+        <div className="outerWrapperSmall">
+          <div style={{ flex: 1 }}>
+            <div className={'imgWrapperSmall'}>
+              <img className={'responsiveImage'} src={resultsToUse.image} alt={'alt'} />
+            </div>
+            {/*{resultsToUse.image &&*/}
+
+            {/*<div className={'image'} style={{backgroundImage: `url("${resultsToUse.image}")`}}/>*/}
+
+            {/*}*/}
+          </div>
+          <div className={'textWrapperSmall'}>
+            <div className={'siteNameLinkWrapper'}>
+              <a href={resultsToUse.url}>{resultsToUse.site_name}</a>
+            </div>
+            <div className={'titleWrapper'}>
+              <p>{resultsToUse.title}</p>
+            </div>
+            <p>{resultsToUse.description}</p>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  passResultsToChildren = () => {
+    const children = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, { ogResults: this.state.result });
+    });
+    return <div>{children}</div>;
+  };
+
+  render() {
+    if (!this.state.result && !this.state.error) {
+      if (this.props.loader) {
+        return this.props.loader;
+      } else {
+        return false;
+      }
+    } else if (this.state.error) {
+      // maybe return an error here?
+      return false;
+    } else {
+      if (this.props.onlyFetch) {
+        return this.passResultsToChildren();
+      } else {
+        let resultsToUse = this.getResultsToUse();
+        console.log('RESULTS TO USE', resultsToUse);
+        const size = this.props.size || 'large';
+        if (size === 'large') {
+          return this.renderLarge(resultsToUse);
+        } else {
+          console.log('renderingSmall');
+          return this.renderSmall(resultsToUse);
+        }
+      }
+    }
+  }
+}
--- another-react-todo-master/src/components/TextLine.js
+++ another-react-todo-master/src/components/TextLine.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
+import './TextLine.css';
+import OpengraphReactComponent from './OpengraphReactComponent';
 
 class TextLine extends Component {
   render() {
     const { text, color } = this.props;
+    const opengraph = [];
     let txt = text;
     let regUrl = /(www\.[\S]+(\b|$))/;
     return (
-      <div style={{ color: color }}>
+      <div className={TextLine} style={{ color: color }}>
         {/* 1차적으로 줄바꿈을 기준으로 나눈다. */}
         {txt.split('\n').map(line => {
           return (
-            <span>
+            <div>
               {/* 2차로 한줄에 있는 텍스트를 정규식을 기준으로 나눠서 각각 처리한다. */}
               {line.split(regUrl).map(url => {
                 if (url.match(regUrl)) {
+                  opengraph.push({ url: url });
                   return (
-                    <a href={'//' + url} target="_blank">
-                      {url}
-                    </a>
+                    <div>
+                      <a href={'//' + url} target="_blank">
+                        {url}
+                      </a>
+                      <OpengraphReactComponent site={url} appId={'0d6683d8-187d-4aa9-a1fb-b43260b087a4'} size={'small'} />
+                    </div>
                   );
                 } else {
                   return url;
                 }
               })}
               <br />
-            </span>
+            </div>
           );
         })}
+        <div>
+          {opengraph.map(metaUrl => {
+            <OpengraphReactComponent site={metaUrl} appId={'0d6683d8-187d-4aa9-a1fb-b43260b087a4'} size={'small'} />;
+          })}
+        </div>
       </div>
     );
   }
