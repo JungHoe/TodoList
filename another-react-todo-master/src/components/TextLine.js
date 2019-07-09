d220cf87f9424a97de39a5848b37965253628198
JungHoe <51188350+JungHoe@users.noreply.github.com>
2019-07-09 14:58:37 +0900

    bugFix
    fix

--- another-react-todo-master/src/components/TextLine.js
+++ another-react-todo-master/src/components/TextLine.js
@@ -30,7 +30,7 @@ class TextLine extends Component {
         {/* 1차적으로 줄바꿈을 기준으로 나눈다. */}
         {txt.split('\n').map(line => {
           return (
-            <div>
+            <div className={` ${checked && 'checked'}`}>
               {/* 2차로 한줄에 있는 텍스트를 정규식을 기준으로 나눠서 각각 처리한다. */}
               {line.split(' ').map(url => {
                 if (url.match(regUrl2)) {
