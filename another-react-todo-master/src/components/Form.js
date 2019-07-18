4219deb77ee9c8a589cc3618747bc6dbe51ba534
JungHoe <51188350+JungHoe@users.noreply.github.com>
2019-07-18 15:35:35 +0900

    업로드 컴포넌트추가
    ㅠㅜ

--- another-react-todo-master/src/components/Form.js
+++ another-react-todo-master/src/components/Form.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Form.css';
+import File from './File'
 
-const Form = ({ value, onChange, onCreate, onKeyPress, color }) => {
+const Form = ({ value, onChange, onCreate, onKeyPress, color,onDrop }) => {
   return (
     <div className="form">
+      <div className="firstWrap">
       <textarea value={value} onChange={onChange} onKeyPress={onKeyPress} style={{ color }} placeholder="오늘은 무엇을 해 볼 까요?" />
       <div className="create-button" onClick={onCreate}>
         추가
       </div>
-    </div>
+      </div>
+      <div className="secondWrap">
+      {<File onDrop={onDrop}></File>}
+      </div>
+     </div>
+    
   );
 };
 
